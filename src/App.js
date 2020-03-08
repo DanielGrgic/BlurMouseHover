809c143e4579e6563ad29934bb4999b6811836c1
Lorenzo <fioriolorenzof@gmail.com>
2020-03-08 10:59:13 +0100

    add blur

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import backImg from './back.jpg';
 
 function App() {
+
+  const [blurPosition, setBlurPosition] = useState({x:-40, y:-40})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BlurDiv 
+        blurPosition={blurPosition}
+        onMouseMove={e => {
+          setBlurPosition({x: e.clientX - e.target.offsetLeft, y: e.clientY - e.target.offsetTop})
+        }}
+      />
     </div>
   );
 }
+
+const BlurDiv = styled.div`
+  margin:50px;
+  width:612px;
+  height:607px;
+  position:relative;    
+  &:before,
+  &:after{
+    content:"";
+    position:absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;    
+    background:url(${backImg});
+  }			
+  &:after {        
+    filter:blur(5px);    
+    clip-path: circle(60px at ${props => (`${props.blurPosition.x}px ${props.blurPosition.y}px)`)};
+  }
+`
 
 export default App;
